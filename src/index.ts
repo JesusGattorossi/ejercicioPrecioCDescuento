2dcbdfd0a97cfb8cc38c97be1c7a893fc3a55966
JesusGattorossi <93558045+JesusGattorossi@users.noreply.github.com>
2022-01-15 01:31:14 -0300

    canbio2

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,14 @@ let porcentajedescuento: number = 0.1;
 let descuento = precioInicial * porcentajedescuento;
 console.log("precio final", precioInicial - descuento);
 */
-/*let botonEnviar = document.getElementById("boton-enviar");
-let precioInicial= document.getElementById("precioInicial");
-let descuento= document.getElementById("descuento");
+let botonEnviar = document.getElementById("boton-enviar");
+let precioInicial = document.getElementById("precioInicial");
+let descuento = document.getElementById("descuento");
 
-botonEnviar?.addEventListener("click",()=>{
-let numeroA: number = Number(precioInicial.value);
-let numeroB : number= Number( descuento.value);
-let  resultado: number= numeroA * (1 - numeroB);
-console.log( " el precio con descuento es:",);
-console.log(resultado);
-
-})
-*/
+botonEnviar?.addEventListener("click", () => {
+  let numeroA: number = Number(precioInicial.value);
+  let numeroB: number = Number(descuento.value);
+  let resultado: number = numeroA * (1 - numeroB);
+  console.log(" el precio con descuento es:");
+  console.log(resultado);
+});
